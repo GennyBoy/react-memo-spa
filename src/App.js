e021c717df2a76265679407da28f10ef1fc20bc6
GennyBoy <tassie2856@gmail.com>
2024-05-31 17:16:16 +0900

    データ追加する機能を実装して該当コードが不要になったので削除

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import './App.css';
 import List from  './List.js';
 
-// TODO: データ追加する機能を実装したら消す
-localStorage.clear();
-localStorage.setItem("memo1", `これはタイトルです
-
-これは3行目です
-これは4行目です`);
-localStorage.setItem("memo2", "メモ2");
-
 export default function App() {
   return (
     <List />
