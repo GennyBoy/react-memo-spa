ed001b595944b81181cdaecb3aab11c502d02607
GennyBoy <tassie2856@gmail.com>
2024-05-31 17:05:23 +0900

    メモ追加機能を実装
    やったこと
    - +ボタンを押したら入力モードに移行する
    - テキストを入力して、「編集」ボタンを押したら、該当メモが一覧に追加される
    
    やってないこと
    - メモ追加後に一覧モードに移行する処理の追加
    - 「削除」ボタンを押下した際の処理の追加
    - レイアウトの調整

--- src/List.js
+++ src/List.js
@@ -1,17 +1,54 @@
+import { useState } from "react";
+
 export default function List() {
-  let memoList = [];
-  for (let i = 0; i < localStorage.length; i++) {
-    const id = localStorage.key(i);
-    const memo = localStorage.getItem(id);
-    const title = memo.split(/\r\n|\n|\r/)[0];
-    memoList.push({
+  const [isEditing, setIsEditing] = useState(false);
+  const [memo, setMemo] = useState('');
+  const [memos, setMemos] = useState(() => {
+    let memoList = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const id = localStorage.key(i);
+      const content = localStorage.getItem(id);
+      const title = content.split(/\r\n|\n|\r/)[0];
+      memoList.push({
+        id: id,
+        title: title,
+        content: content
+      });
+    }
+    return memoList;
+  });
+
+  // TODO: localStorageのオブジェクトを、メモアプリの世界のオブジェクトに変換する関数作成する
+
+  function handleEditButtonClick(e) {
+    const id = window.self.crypto.randomUUID();
+    const content = memo;
+    const title = content.split(/\r\n|\n|\r/)[0];
+    localStorage.setItem(id, content);
+    setMemos([...memos, {
       id: id,
       title: title,
-      memo: memo
-    });
+      content: content
+    }]);
   }
 
+// TODO : 編集ボタンをクリックしたら一覧の状態に戻るようにする
+
+
   return (
-    <ul>{memoList?.map(item => <li key={item.id}>{item.title}</li>)}</ul>
+    <>
+      <ul>{memos?.map(memo => <li key={memo.id}>{memo.title}</li>)}</ul>
+      <a href="." onClick={e => {
+        e.preventDefault();
+        setIsEditing(true);
+      }}>+</a>
+      {isEditing &&
+        <div>
+          <textarea onChange={e => setMemo(e.target.value)}></textarea>
+          <button onClick={handleEditButtonClick}>編集</button>
+          <button>削除</button>
+        </div>
+      }
+    </>
   )
 }
