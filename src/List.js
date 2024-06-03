37e560903b0170fe15c98a9114d45aa84f0c6a4f
GennyBoy <tassie2856@gmail.com>
2024-06-03 12:09:31 +0900

    メモの編集機能を追加
    - `memo` state の値をオブジェクトに変更して `id` も持たせるようにした
      - 編集時のデータ更新に必要
    - `isAdding` state を導入
      - `handleEditButtonClick` 内の処理を編集と新規追加で分けた

--- src/List.js
+++ src/List.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 
 export default function List() {
   const [isEditing, setIsEditing] = useState(false);
-  const [memo, setMemo] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+  const [memo, setMemo] = useState({id: null, content: ''});
   const [memos, setMemos] = useState(() => {
     let memoList = [];
     for (let i = 0; i < localStorage.length; i++) {
@@ -23,33 +24,66 @@ export default function List() {
   }
 
   function handleEditButtonClick(e) {
-    if (!memo) {
+    if (!memo.content) {
       alert("メモを入力してください");
       return;
     }
-    const id = window.self.crypto.randomUUID();
-    const content = memo;
+    // 編集の時はidが既にあるのでそれを代入し、新規の時はランダムUUIDを生成して代入する
+    const id = memo.id || window.self.crypto.randomUUID();
+    const content = memo.content;
     const title = extractTitleFromContent(content);
     localStorage.setItem(id, content);
-    setMemos([...memos, {
-      id: id,
-      title: title,
-      content: content
-    }]);
-    setMemo('');
-    setIsEditing(false);
+
+    if (isEditing) {
+      const newMemos = memos.map((memo) => {
+        if (memo.id === id) {
+          return {
+            id: id,
+            title: title,
+            content: content
+          }
+        } else {
+          return memo;
+        }
+      });
+      setMemos(newMemos);
+      setIsEditing(false);
+    }
+
+    if (isAdding) {
+      setMemos([...memos, {
+        id: id,
+        title: title,
+        content: content
+      }]);
+      setIsAdding(false);
+    }
+
+    setMemo({id: null, content: ''});
   }
 
   return (
     <>
-      <ul>{memos?.map(memo => <li key={memo.id}>{memo.title}</li>)}</ul>
+      <ul>
+        {memos?.map(memo => {
+          return (
+            <li key={memo.id}>
+              <a href="." onClick={(e) => {
+                e.preventDefault();
+                setMemo({id: memo.id, content: localStorage.getItem(memo.id)});
+                setIsEditing(true);
+              }}>{memo.title}</a>
+            </li>
+          );
+        })}
+      </ul>
       <a href="." onClick={e => {
         e.preventDefault();
-        setIsEditing(true);
+        setIsAdding(true);
       }}>+</a>
-      {isEditing &&
+      {(isEditing || isAdding) &&
         <div>
-          <textarea onChange={e => setMemo(e.target.value)}></textarea>
+          <textarea value={memo.content || ''} onChange={e => setMemo({id: memo.id || null, content: e.target.value})}></textarea>
           <button onClick={handleEditButtonClick}>編集</button>
           <button>削除</button>
         </div>
